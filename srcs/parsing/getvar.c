72634308e820f39276baa3d9b68ca065621c9a6b
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-05-17 11:50:48 -0400

    Update getvar.c

--- srcs/parsing/getvar.c
+++ srcs/parsing/getvar.c
@@ -13,6 +13,12 @@
 #include "builtins.h"
 #include <string.h>
 
+/*char *ms_expand_cmd(char *str, int i)
+{
+
+
+}*/
+
 static char *ms_expand(char *str, int i)
 {
 	int j;
@@ -23,17 +29,38 @@ static char *ms_expand(char *str, int i)
 	j = 0;
 	while(str[i + j] && str[i + j] != ' ' && str[i + j] != '$' && str[i + j] != '\'' && str[i+j] != '"')
 		j++;
-	var = env_var_get_value(ft_substr(str, i, j), j);
-	if (var == NULL)
-		return str;
+	printf("i = %d, j = %d\n", i, j);
+	if (str[i-1] == '$' && str[j] == '?') {
+		j = 0;
+		while (str[i] && str[i] != '$')
+			i++;
+		i++;
+		while(str[i + j] && str[i + j] != ' ' && str[i + j] != '|' && str[i + j] != '\'' && str[i+j] != '"')
+			j++;
+	}
 	tmp = ft_substr(str, 0, i - 1);
-	res = ft_strjoin(ft_strjoin(tmp, var), &str[i+j]);
+	printf("tmp:%s ", tmp);
+	var = env_var_get_value(ft_substr(str, i, j), j);
+	printf("var:%s\n", var);
+	printf("i = %d, j = %d\n", i, j);
+	if (var == NULL)
+		res = ft_strjoin(tmp, &str[i + j]);
+	else
+		res = ft_strjoin(ft_strjoin(tmp, var), &str[i + j]);
+	printf("res = '%s', strlen = %i\n", res, ft_strlen(res));
 	free(tmp);
 	return (res);
 }
 
 char *get_var(char *str, int quote, int i)
 {
+	int j;
+	int b;
+	int c;
+
+	j = 0;
+	b = 0;
+	c = 0;
 	while (str[++i] && str[i + 1])
 	{
 		if (str[i] == '\'')
@@ -41,8 +68,21 @@ char *get_var(char *str, int quote, int i)
 			quote *= -1;
 			i++;
 		}
-		if (str[i] == '$' && str[i + 1] && quote > 0)
-			return (get_var(ms_expand(str, ++i), quote, -1));
+		while (str[j])
+		{
+			if (str[j] == '$')
+			{
+				b++;
+				if (str[j + 1] && str[j + 1] == '?')
+					c++;
+			}
+			j++;
+		}
+		printf("str = %s, b = %d, c = %d\n", str, b, c);
+		if (b == c)
+			return (str);
+		else if (str[i] == '$' && str[i++] && quote > 0)
+			return (get_var(ms_expand(str, i), quote, i));
 	}
 	return (str);
 }
